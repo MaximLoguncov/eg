b24759078bcc66babc6ac1239139c8489d86cf57
beryllos <loguncovmaxim@gmail.com>
2018-07-01 11:28:34 +0300

    start screen

---
+++ eg/Assets/Scripts/Buttons.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buttons : MonoBehaviour {
+
+	// Use this for initialization
+	void OnMouseDown (){
+				transform.localScale = new Vector3 (0.55f, 0.55f, 0.55f);
+			}
+	void OnMouseUP (){
+				transform.localScale = new Vector3 (0.5f, 0.5f, 0.5f);
+}
+}
