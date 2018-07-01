57e5783d26a6774e1ab62bd27502a0715638719c
beryllos <loguncovmaxim@gmail.com>
2018-07-01 15:47:07 +0300

    button

--- eg/Assets/Scripts/Buttons.cs
+++ eg/Assets/Scripts/Buttons.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class Buttons : MonoBehaviour {
 
-	// Use this for initialization
+
 	void OnMouseDown (){
-				transform.localScale = new Vector3 (0.55f, 0.55f, 0.55f);
+				transform.localScale = new Vector3 (0.025f, 0.025f, 0.025f);
 			}
-	void OnMouseUP (){
-				transform.localScale = new Vector3 (0.5f, 0.5f, 0.5f);
+
+	void OnMouseUp (){
+				transform.localScale = new Vector3 (0.019f, 0.019f, 0.019f);
 }
 }
