93fa889cfd87f2748124a59440ec6b33a1aef0d1
beryllos <loguncovmaxim@gmail.com>
2018-07-05 18:20:54 +0300

    2d

---
+++ eg/Assets/Scripts/scrollObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrollObject : MonoBehaviour {
+
+	public float speed = 5f, checkPos = 0f;
+	private RectTransform rec;
+
+	void Start () {
+		rec = GetComponent <RectTransform> ();
+	}
+
+	void Update () {
+		if (rec.offsetMin.y != checkPos) {
+				rec.offsetMin += new Vector2 (rec.offsetMin.x, speed);
+				rec.offsetMax += new Vector2 (rec.offsetMax.x, speed);
+
+				if (rec.offsetMax.y != checkPos) {
+						rec.offsetMin += new Vector2 (rec.offsetMin.x, speed);
+						rec.offsetMax += new Vector2 (rec.offsetMax.x, speed);
+				}
+		}
+	}
+}
